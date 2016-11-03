f33bc95c4cea575c491a0da4c19186576492a57f
Shankar <you@example.com>
2016-11-03 09:57:31 +0530

    - Route Implementation; ;

--- myBlog/urls.py
+++ myBlog/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^', views.index, name='index')
+    # Main Root
+    url(r'^$', views.index, name='index'),
+    # Sub Root
+    url(r'^/welcome/$', views.welcome, name='welcome')
 ]
